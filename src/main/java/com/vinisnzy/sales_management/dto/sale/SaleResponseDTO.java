79dd22122895b779e3f60a943e9d16fed718f35a
Vinícius de Paula <manoseilacara123@gmail.com>
2025-08-10 21:32:39 -0300

    fix: change total to BigDecimal, items to List and add status to SaleResponseDTO

--- src/main/java/com/vinisnzy/sales_management/dto/sale/SaleResponseDTO.java
+++ src/main/java/com/vinisnzy/sales_management/dto/sale/SaleResponseDTO.java
@@ -1,12 +1,17 @@
 package com.vinisnzy.sales_management.dto.sale;
 
+import com.vinisnzy.sales_management.enums.SaleStatus;
+
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public record SaleResponseDTO(
         Long id,
         String clientName,
-        SaleItemResponseDTO[] items,
-        Double total,
+        List<SaleItemResponseDTO> items,
+        BigDecimal total,
+        SaleStatus status,
         LocalDateTime createdAt
 ) {
 }
