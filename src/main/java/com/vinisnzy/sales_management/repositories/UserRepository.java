7c08fa4e9d3fe139342815d6863dfd6b13035761
Vinícius de Paula <manoseilacara123@gmail.com>
2025-08-14 14:27:59 -0300

    feat: create findByEmail method in UserRepository

--- src/main/java/com/vinisnzy/sales_management/repositories/UserRepository.java
+++ src/main/java/com/vinisnzy/sales_management/repositories/UserRepository.java
@@ -3,5 +3,8 @@ package com.vinisnzy.sales_management.repositories;
 import com.vinisnzy.sales_management.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
