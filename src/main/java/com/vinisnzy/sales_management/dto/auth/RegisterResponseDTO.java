505038b509c32edb8c14469041a89f5020172301
Vinícius de Paula <manoseilacara123@gmail.com>
2025-08-14 14:27:10 -0300

    fix: remove password field from RegisterResponseDTO

--- src/main/java/com/vinisnzy/sales_management/dto/auth/RegisterResponseDTO.java
+++ src/main/java/com/vinisnzy/sales_management/dto/auth/RegisterResponseDTO.java
@@ -2,7 +2,6 @@ package com.vinisnzy.sales_management.dto.auth;
 
 public record RegisterResponseDTO(
         String name,
-        String email,
-        String password
+        String email
 ) {
 }
